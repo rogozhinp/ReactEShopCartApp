fc0d4cfa96374945365cc94aea6adbe2ae1e30c3
Pavel <rogozhin.p@yandex.ru>
2019-12-12 02:29:53 +0300

    show cards without pictures

--- src/App.js
+++ src/App.js
@@ -4,32 +4,37 @@ import "./App.css";
 import Products from "./components/Products";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { products:[], filteredProducts: []};
-}
-  componentWillMount{
-    fetch("http://localhost:8000/products").then(res => res.json())
-    .then(data => this.setState({
-      products: data,
-      filteredProducts: data
-  }));
-  render(){
-  return(
-  <div className="container">
-    <h1>Ecommerce Shopping Cart Application</h1>
-    <hr />
-    <div className="row">
-      <div className="col-md-8">
-        <Products
-          products={this.state.filteredProducts}
-          handleAddToCart={this.handleAddToCart}
-        />
+    this.state = { products: [], filteredProducts: [] };
+  }
+  componentWillMount() {
+    fetch("http://localhost:8000/products")
+      .then(res => res.json())
+      .then(data =>
+        this.setState({
+          products: data,
+          filteredProducts: data
+        })
+      );
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Ecommerce Shopping Cart Application</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-8">
+            <Products
+              products={this.state.filteredProducts}
+              handleAddToCart={this.handleAddToCart}
+            />
+          </div>
+          <div className="col-md-4"></div>
+        </div>
       </div>
-      <div className="col-md-4"></div>
-    </div>
-  </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 export default class Products extends Component {
   render() {
-    return <div></div>;
+    const productItems = this.props.products.map(product => (
+      <div className="col-md-4">
+        <div className="thumbnail text-center">
+          <a href={"#${product.id}"} onClick={this.props.handleAddToCard}>
+            <img src={"/products/${product.sku}.jpg"} alt={product.title} />
+            <p>{product.title}</p>
+          </a>
+        </div>
+      </div>
+    ));
+    return <div className="row">{productItems}</div>;
   }
 }
