b0a5152afa2b7ed5c40374aacedaac318f7f725a
Pavel <rogozhin.p@yandex.ru>
2019-12-12 12:44:21 +0300

    add price and Add to Cart button

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import util from '../util'
 
 export default class Products extends Component {
   render() {
     const productItems = this.props.products.map(product => (
       <div className="col-md-4">
         <div className="thumbnail text-center">
-          <a href={"#${product.id}"} onClick={this.props.handleAddToCard}>
-            <img src={"/products/${product.sku}.jpg"} alt={product.title} />
+          <a href={'#${product.id}'} onClick={(e)=>this.props.handleAddToCard(e, product)}>
+                <img src={'/products/876661122392077_2.jpg'} alt={product.title} />
             <p>{product.title}</p>
           </a>
+          <div>
+                <b>{util.formatCurrency(product.price)}</b>
+                <button className="btn btn-primary"
+                onClick={(e)=>this.props.handleAddToCard(e, product)}>Add to Cart</button>
+          
+          </div>
         </div>
       </div>
     ));
-    return <div className="row">{productItems}</div>;
+    return (
+
+        <div className="row">
+             {productItems}
+        </div>
+        
+        )
   }
 }
